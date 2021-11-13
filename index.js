c8d4d37cb828a76584e5ad991c46099bbcc16b56
nhatnn <nhatnn@nhatnns-iMac.local>
2021-11-13 23:59:18 +0700

    [update]

--- index.js
+++ index.js
@@ -24,3 +24,91 @@ blinkTextMenuLinks.forEach((link) => {
     link.append(span);
   });
 });
+
+const content_astronaut = document.getElementById("background-video_concept")
+
+if (content_astronaut) {
+  content_astronaut.volume = 0
+  content_astronaut.play()
+}
+
+// handle typer
+
+var sourceCode = ""
+var sourceContainer, sourceElement, accessMessageElement;
+
+var startIndex = 0;
+var endIndex = 0;
+var cursorChar = "|";
+
+var locked = false;
+
+const CHARS_PER_STROKE = 1;
+const load_source_code = () => {
+  var client = new XMLHttpRequest();
+  client.open('GET', './code.txt');
+  client.onreadystatechange = function () {
+    sourceCode = client.responseText;
+  }
+  client.send();
+}
+
+const getElements = () => {
+  sourceContainer = document.getElementById("container");
+  sourceElement = document.getElementById("source");
+  accessMessageElement = document.getElementById("access-msg")
+}
+
+const update_screen = () => {
+  if (!locked) {
+    endIndex += CHARS_PER_STROKE;
+    sourceElement.textContent = sourceCode.substring(startIndex, endIndex);
+
+    //scroll position
+    // window.scrollTo(0, sourceContainer.scrollHeight);
+    // update_cursor
+    sourceElement.textContent += cursorChar;
+    console.log(sourceElement.textContent);
+    // update access message
+  }
+
+}
+
+function update_cursor() {
+  var text = sourceElement.textContent;
+  // var lastChar = text.charAt(text.length - 1);
+  // console.log(lastChar, cursorChar);
+  // if (lastChar === cursorChar) {
+  //     sourceElement.textContent = text.substring(0, text.length - 1);
+  // }
+  // else {
+  sourceElement.textContent += cursorChar;
+  // }
+}
+
+const remove_message = () => {
+  locked = false;
+  accessMessageElement.removeAttribute('class')
+  sourceContainer.removeAttribute('class')
+}
+
+const init = () => {
+  load_source_code()
+  getElements()
+  window.setTimeout(update_cursor, 500);
+}
+
+init()
+
+window.onkeydown = (e) => {
+  if (e.key === "Escape")
+    remove_message()
+  else
+    update_screen()
+};
+
+window.onload = () => {
+  setInterval(() => {
+    update_screen()
+  }, 100);
+}
